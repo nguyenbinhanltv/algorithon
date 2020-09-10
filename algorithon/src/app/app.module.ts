c125459e553efbe5d7e467de3cabb6b784d58cfa
Nguyễn Bình An <nguyenbinhanltv@gmail.com>
2020-09-11 00:37:39 +0700

    update zorro-antd

--- algorithon/src/app/app.module.ts
+++ algorithon/src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ComponentsModule } from './components/components.module';
 import { RouterModule } from '@angular/router';
+
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -16,9 +26,13 @@ import { RouterModule } from '@angular/router';
     AppRoutingModule,
     ComponentsModule,
     ReactiveFormsModule,
-    RouterModule
+    RouterModule,
+    NzButtonModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
